78d99939607871ebdcc068607762bcc9b3aba496
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-12-10 11:09:45 -0300

    Create new Entity CreditCard and Tests

--- process-transaction/domain/entity/credit_card_test.go
+++ process-transaction/domain/entity/credit_card_test.go
@@ -1,12 +1,32 @@
 package entity
 
 import (
-	"testing"
-
 	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
 )
 
 func TestCreditCardNumber(t *testing.T) {
-	creditCard, err := NewCreditCard("4000000000000000", "José da Silva", 12, 2024, 123)
-	assert.Equal(t, "invalid credit card number", err.Error())
+	_, err := NewCreditCard("40000000000000000", "José da Silva", 12, 2024, 123)
+	assert.Equal(t, "Invalid credit card number", err.Error())
+
+	_, err = NewCreditCard("4193523830170205", "José da Silva", 12, 2024, 123)
+	assert.Nil(t, err)
+}
+
+func TestCreditCardExpirationMonth(t *testing.T) {
+	_, err := NewCreditCard("4193523830170205", "José da Silva", 13, 2024, 123)
+	assert.Equal(t, "Invalid expiration month", err.Error())
+
+	_, err = NewCreditCard("4193523830170205", "José da Silva", 0, 2024, 123)
+	assert.Equal(t, "Invalid expiration month", err.Error())
+
+	_, err = NewCreditCard("4193523830170205", "José da Silva", 11, 2024, 123)
+	assert.Nil(t, err)
+}
+
+func TestCreditCardExpirationYear(t *testing.T) {
+	lastYear := time.Now().AddDate(-1, 0, 0)
+	_, err := NewCreditCard("4193523830170205", "José da Silva", 12, lastYear.Year(), 123)
+	assert.Equal(t, "Invalid expiration year", err.Error())
 }
